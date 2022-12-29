1ceef5628bbd4159ae72a783db5c5b31e936d1e7
ners <ners@gmx.ch>
2022-12-29 23:33:59 +0100

    fix cross-compilation

--- flake.nix
+++ flake.nix
@@ -1,29 +1,53 @@
 {
-  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-22.11;
+  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+  inputs.nixpkgs-musl.url = github:ners/nixpkgs/riscv-musl;
   inputs.flake-utils.url = github:numtide/flake-utils;
 
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (hostSystem:
+  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (buildPlatform:
     let
-      pkgsFor = sys: inputs.nixpkgs.legacyPackages.${sys};
-      hostPkgs = pkgsFor hostSystem;
-      targetSystem = "riscv64-linux";
-      targetPkgs = pkgsFor targetSystem;
-      inherit (inputs.nixpkgs) lib;
+      pkgsFor = nixpkgs: system: import nixpkgs { inherit system; };
+      buildPkgs = pkgsFor inputs.nixpkgs buildPlatform;
+      hostPlatform = "riscv64-linux";
     in
     {
-      nixosConfigurations.MangoPi = lib.nixosSystem {
-        system = targetSystem;
-        modules = [
-          ./mangoPi.nix
-        ];
+      nixosConfigurations = {
+        MangoPi =
+          let nixpkgs = inputs.nixpkgs; in
+          nixpkgs.lib.nixosSystem {
+            modules = [
+              {
+                nixpkgs = { inherit buildPlatform hostPlatform; };
+              }
+              ./mangoPi.nix
+            ];
+          };
+        MangoPiMusl =
+          let nixpkgs = inputs.nixpkgs-musl; in
+          nixpkgs.lib.nixosSystem {
+            modules = [
+              {
+                nixpkgs = {
+                  inherit buildPlatform hostPlatform;
+                  pkgs = (pkgsFor nixpkgs hostPlatform).pkgsMusl;
+                };
+              }
+              ./mangoPi.nix
+            ];
+          };
       };
 
-      devShells.default = hostPkgs.mkShell { };
+      devShells.default = buildPkgs.mkShell { };
 
-      packages = rec {
-        inherit (inputs.self.nixosConfigurations.${hostSystem}.MangoPi.config.system.build) sdImage toplevel;
-        default = sdImage;
-      };
+      packages =
+        let
+          nixpkgs = inputs.nixpkgs;
+          pkgs = pkgsFor nixpkgs buildPlatform;
+        in
+        rec {
+          inherit (inputs.self.nixosConfigurations.${buildPlatform}.MangoPi.config.system.build) sdImage toplevel;
+          default = sdImage;
+          sdImageMusl = inputs.self.nixosConfigurations.${buildPlatform}.MangoPiMusl.config.system.build.sdImage;
+        };
     }
   );
 }
